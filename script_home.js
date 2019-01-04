5e0b7d750f6d1cc13c82860144ee5fcfab1f7346
Jannick Holm <jannick@wonderfestiwall.dk>
2019-01-04 10:37:17 +0100

    Friday 4 January, 10.37

---
+++ script_home.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", sidenVises);
+
+
+function sidenVises() {
+    console.log("Siden blev loadet uden issues");
+    document.querySelector(".preload_home").style.display = "none";
+}
--- script_stilart.js
+++ script_stilart.js
@@ -3,5 +3,5 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log("Siden blev loadet uden issues");
-    document.querySelector(".preload").style.display = "none";
+    document.querySelector(".preload_stilart").style.display = "none";
 }

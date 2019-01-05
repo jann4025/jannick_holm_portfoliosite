cb901799171c04dcf2b8b2fc7856389653527136
Jannick Holm <jannick@wonderfestiwall.dk>
2019-01-05 19:43:01 +0100

    5.

---
+++ script_redesign.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("Siden blev loadet uden issues");
+    document.querySelector(".preload_redesign").style.display = "none";
+}
--- script_stilart.js
+++ script_stilart.js
@@ -1,6 +1,5 @@
 window.addEventListener("load", sidenVises);
 
-
 function sidenVises() {
     console.log("Siden blev loadet uden issues");
     document.querySelector(".preload_stilart").style.display = "none";
